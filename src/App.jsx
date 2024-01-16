1177a1f9396b4e976ed0f8bad1bdf04ca403b147
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-01-16 16:08:11 +0200

    GET Token from TMDB API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
 import './App.css'
+import {fetchData} from './utils/api.js';
 
 function App() {
-  
+  useEffect(()=>{
+    moviesPopular();
+  },[]);
+
+  const moviesPopular = ()=>{
+    fetchData('movie/popular')
+  }
+
   return (
     <div>
       Welcome Movies
