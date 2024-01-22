5626d43b977502a2faef7572b068c4b235c09614
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-01-22 18:28:26 +0200

    Complete Cast Section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,15 +2,17 @@ import { useParams } from 'react-router-dom';
 
 import DetailsBanner from './detailsBanner/DetailsBanner';
 import useFetch from '../../hooks/useFetch';
+import Cast from './cast/Cast';
 
 const Details = () => {
   const {mediaType, id} = useParams();
-  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data } = useFetch(`/${mediaType}/${id}/videos`);
   const { data: credits, loading:creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
 
   return (
     <div className='detailsPage'>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
+      <Cast data={credits?.cast} loading={creditsLoading} />
     </div>
   )
 }
