fce199b441d9436738dd1ce33a59df57c8c515c9
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-01-22 18:48:06 +0200

    Complete Official Videos Section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,16 +3,18 @@ import { useParams } from 'react-router-dom';
 import DetailsBanner from './detailsBanner/DetailsBanner';
 import useFetch from '../../hooks/useFetch';
 import Cast from './cast/Cast';
+import Videos from './videos/Videos';
 
 const Details = () => {
   const {mediaType, id} = useParams();
-  const { data } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
   const { data: credits, loading:creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
 
   return (
     <div className='detailsPage'>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
       <Cast data={credits?.cast} loading={creditsLoading} />
+      <Videos data={data} loading={loading} />
     </div>
   )
 }
